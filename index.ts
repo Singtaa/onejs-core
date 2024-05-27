f10e12a636d6e407710845944028fea76e5a78e6
David <st0724@gmail.com>
2024-05-27 14:01:22 -0700

    export h() function
    had to strip the typings to make jsr happy

--- index.ts
+++ index.ts
@@ -6,32 +6,30 @@
 //     }
 // }
 
-// declare var document: any;
+declare var document: any
 
-// export const tmp = 123
+export function h(type: any, props: any, ...children: any[]): any {
+    const element = typeof type === "string" ? document.createElement(type) : type;
 
-// export function h(type: any, props: any, ...children: (CS.OneJS.Dom.Dom | string)[]): CS.OneJS.Dom.Dom {
-//     const element = typeof type === "string" ? document.createElement(type) : type;
+    // Assign properties to the element
+    for (const [key, value] of Object.entries(props || {})) {
+        if (key.startsWith("on") && typeof value === "function") {
+            element.addEventListener(key.substring(2).toLowerCase(), value);
+        } else if (key === "style" && typeof value === "object") {
+            Object.assign(element.style, value);
+        } else {
+            element.setAttribute(key, value);
+        }
+    }
 
-//     // Assign properties to the element
-//     for (const [key, value] of Object.entries(props || {})) {
-//         if (key.startsWith("on") && typeof value === "function") {
-//             element.addEventListener(key.substring(2).toLowerCase(), value);
-//         } else if (key === "style" && typeof value === "object") {
-//             Object.assign(element.style, value);
-//         } else {
-//             element.setAttribute(key, value);
-//         }
-//     }
+    // Append children
+    for (const child of children) {
+        if (typeof child === "string") {
+            element.appendChild(document.createTextNode(child));
+        } else {
+            element.appendChild(child);
+        }
+    }
 
-//     // Append children
-//     for (const child of children) {
-//         if (typeof child === "string") {
-//             element.appendChild(document.createTextNode(child));
-//         } else {
-//             element.appendChild(child);
-//         }
-//     }
-
-//     return element;
-// }
+    return element;
+}
