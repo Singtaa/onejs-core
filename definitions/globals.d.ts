0b8a86b399e268020bda302b28bc953a8a357ac9
David <st0724@gmail.com>
2024-05-27 20:39:52 -0700

    removed global self def due to jsr

--- definitions/globals.d.ts
+++ definitions/globals.d.ts
@@ -25,7 +25,6 @@ declare global {
 
     var document: CS.OneJS.Dom.Document;
     var resource: CS.OneJS.Resource;
-    var self: any;
 
     function atob(encodedData: string): string;
     function btoa(rawData: string): string;
