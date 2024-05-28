028d7902963a5ed3fe17113d03751444ea6afd6f
David <st0724@gmail.com>
2024-05-28 13:20:24 -0700

    trying @ts-ignore fix for allowImportingTsExtensions error

--- utils/index.ts
+++ utils/index.ts
@@ -4,6 +4,9 @@
  * This module contains some commonly-used utility functions.
  */
 
+// @ts-ignore - prevent `allowImportingTsExtensions` error
 export { parseColor, parseCSSColor, colorStrToF4, namedColor, namedColors } from "./color-parser"
+// @ts-ignore - prevent `allowImportingTsExtensions` error
 export { palettes } from "./color-palettes"
+// @ts-ignore - prevent `allowImportingTsExtensions` error
 export { parseFloat2, parseFloat3, parseFloat4 } from "./float-parser"
