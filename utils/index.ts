7298aa86d612414eabfcf6d475c7fc646b93e523
David <st0724@gmail.com>
2024-05-28 02:04:28 -0700

    added missing parseColor

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,3 @@
-export { parseCSSColor, namedColor, namedColors } from "./color-parser"
+export { parseColor, parseCSSColor, colorStrToF4, namedColor, namedColors } from "./color-parser"
 export { palettes } from "./color-palettes"
 export { parseFloat2, parseFloat3, parseFloat4 } from "./float-parser"
