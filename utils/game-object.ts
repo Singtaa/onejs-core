10606a089fa3a58ee37057684d3a8bccf6f19762
David <st0724@gmail.com>
2024-05-28 00:46:11 -0700

    tsconfig tweak

--- utils/game-object.ts
+++
@@ -1,9 +0,0 @@
-const { $typeof } = puer
-
-export function getComp<T>(obj: any, type: { new(): T }): T {
-    return obj.GetComponent($typeof(type)) as T
-}
-
-export function addComp<T>(obj: any, type: { new(): T }): T {
-    return obj.AddComponent($typeof(type)) as T
-}
---
+++ utils/index.ts
@@ -0,0 +1,3 @@
+export { parseCSSColor, namedColor, namedColors } from "./color-parser"
+export { palettes } from "./color-palettes"
+export { parseFloat2, parseFloat3, parseFloat4 } from "./float-parser"
