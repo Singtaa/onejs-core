c574e20e5f32dece933bebdf6040aefafd13f124
David <st0724@gmail.com>
2024-05-27 22:31:45 -0700

    extract namespace exports into modules.d.ts

--- definitions/globals.d.ts
+++ definitions/globals.d.ts
@@ -53,34 +53,4 @@ declare global {
 
 }
 
-declare module "OneJS" {
-    export = CS.OneJS
-}
-
-declare module "OneJS/Utils" {
-    export = CS.OneJS.Utils
-}
-
-declare module "OneJS/Dom" {
-    export = CS.OneJS.Dom
-}
-
-declare module "UnityEngine" {
-    export = CS.UnityEngine;
-}
-
-declare module "UnityEngine/UIElements" {
-    export = CS.UnityEngine.UIElements;
-}
-
-declare module "Unity/Mathematics" {
-    export = CS.Unity.Mathematics;
-}
-
-import puerts = puer;
-
-declare module "puerts" {
-    export = puerts;
-}
-
-export { };
+export { }
