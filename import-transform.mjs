0950877203b3a1e1a2115e636b4bf87a07f21fa3
Dave <st0724@gmail.com>
2024-06-24 18:22:59 -0400

    moved onejs-import-transform here

---
+++ import-transform.mjs
@@ -0,0 +1,42 @@
+import * as fs from "fs";
+import * as path from "path";
+
+export const importTransformPlugin = {
+	name: "onejs-import-transform",
+	setup(build) {
+		// First pass: Mark C# imports as external
+		build.onResolve({ filter: /^(UnityEngine|OneJS)/ }, (args) => {
+			return { path: args.path, external: true };
+		});
+
+		// Second pass: Transform all JS and TSX files
+		build.onLoad({ filter: /\.(js|tsx)$/ }, async (args) => {
+			let contents = await fs.promises.readFile(args.path, "utf8");
+
+			// Transform Unity imports
+			contents = contents.replace(
+				/import\s+(?:{([^}]+)})?\s*from\s*["'](UnityEngine[^"']*|OneJS[^"']*)["'];?/g,
+				(match, imports, moduleName) => {
+					moduleName = moduleName.replace(/\//g, ".");
+					if (imports) {
+						const importItems = imports.split(",").map((item) => item.trim());
+						return `const { ${importItems.join(", ")} } = CS.${moduleName};`;
+					} else {
+						const namespaceName = moduleName.split(".").pop();
+						return `const ${namespaceName} = CS.${moduleName};`;
+					}
+				}
+			);
+
+			// Transform any remaining require statements for Unity modules
+			contents = contents.replace(
+				/__require\(["'](UnityEngine[^"']*|OneJS[^"']*)["']\)/g,
+				(match, moduleName) => {
+					return `CS.${moduleName.replace(/\//g, ".")}`;
+				}
+			);
+
+			return { contents, loader: path.extname(args.path).slice(1) };
+		});
+	},
+};
