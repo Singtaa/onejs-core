feeb81856059c398405cf2116fc30e58010ab22c
David <st0724@gmail.com>
2024-08-01 02:08:11 -0700

    watcher output tweaks

--- import-transform.mjs
+++ import-transform.mjs
@@ -40,3 +40,23 @@ export const importTransformPlugin = {
 		});
 	},
 };
+
+export const watchOutputPlugin = {
+	name: "onejs-watch-output",
+	setup(build) {
+		build.onStart(() => {
+			// Clear the terminal
+			process.stdout.write('\x1Bc');
+			console.log("[esbuild] starting build...");
+		});
+
+		build.onEnd((result) => {
+			// if (result.errors.length > 0) {
+			//     console.error("[esbuild] build failed");
+			// } else {
+			//     console.log("[esbuild] build succeeded");
+			// }
+			console.log("[esbuild] watching...");
+		});
+	}
+}
